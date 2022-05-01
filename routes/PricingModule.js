194e7a97856a52e7828b1e8bd0195e78dfddd22f
Jordan Miles <98437026+JordMiles064@users.noreply.github.com>
2022-05-01 13:36:58 -0500

    Add files via upload

---
+++ routes/PricingModule.js
@@ -0,0 +1,20 @@
+module.exports = {calculate: (fuelQuotes, gallonsRequested, inState, pricePerGallon) => {
+    //fuelQuotes: List of clients' previous fuel quotes, if any, as a list of objects.
+    //
+    //gallonsRequested: The amount of gallons requested by the client, numerical.
+    //
+    //inState: Whether the client is in or out of state, true or false boolean.
+    //
+    //pricePerGallon: The set price/gallon, numerical.
+
+    let total = 1;
+    
+    console.log("Starting calculation in pricing module...");
+    console.log(fuelQuotes);
+
+    //
+    //TODO: Calculate total using the function paramters
+    //
+
+    return total;
+}};
